eb57e1ecc23322d2bb82844b835560260608da37
pHelics <contact@phelics.com>
2013-11-26 18:25:38 -0500

    Added twitter module

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var app = express();
 var sentimental = require('Sentimental');
 var ejs = require('ejs');
 
+app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
-console.log('Listening at http://localhost:3000');
+console.log('Server running at http://localhost:3000');
