1fa5f9ab33f1e5e930601ae50a938a57518d92c9
pHelics <contact@phelics.com>
2013-11-26 19:41:15 -0500

    Created skeleton for EJS

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,9 @@
 exports.index = function(req, res){
-    
+    res.render('index.ejs', {title: 'TNT Main'}, function(err, stuff){
+    	if (!err){
+    		console.log(stuff);
+    		res.write(stuff);
+    		res.end();
+    	}
+    });
 };
--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-var express = require('express');
+var express = require('express'),
+	routes = require('./routes'),
+	http = require('http'),
+	sentimental = require('Sentimental'),
+	twitter = require('twitter');
 var app = express();
 
 var sentimental = require('Sentimental');
 var ejs = require('ejs');
 
-app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
+app.configure(function(){
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.bodyParser());
+  	app.use(express.methodOverride());
+  	app.use(app.router);
+});
 
-app.listen(3000);
+app.get('/', routes.index);
+
+http.createServer(app).listen(3000);
+
 console.log('Server running at http://localhost:3000');
