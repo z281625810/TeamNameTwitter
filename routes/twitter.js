96240be55c907ec5afa68bd4643469e754ab636d
pHelics <contact@phelics.com>
2013-11-30 04:32:40 -0500

    Twit form search displays results now

--- routes/twitter.js
+++ routes/twitter.js
@@ -22,21 +22,41 @@ exports.search = function(req, res){
 exports.search = function(req, res){
 	
 	var hashtags = req.body.hashtags,
+		date = req.body.date,
 		radius = req.body.radius,
 		happyTweets = req.body.sentimentalTweets ? true : false,
 		formSubmitted = true;
 
 	console.log('hashtags: '+hashtags);
+	console.log('date: '+date);
 	console.log('radius: '+radius);
 	console.log('happyTweets: '+happyTweets);
 	console.log('formSubmitted: ' + formSubmitted);
 
 	// Checks to see if user entered at least 2 hashtags
-	check(hashtags, 'Please enter at least 2 hashtags').regex(/^([a-zA-Z0-9]+(\s?)){2,10}$/i);
+	//check(hashtags, 'Please enter at least 2 hashtags').regex(/^([a-zA-Z0-9]+(\s?)){2,10}$/i);
 
-	T.get('search/tweets', { q: 'banana since:2011-11-11', count: 100 }, function(err, reply) {
-		//console.log(util.inspect(reply));
-		//console.log('Error: ' + err);
+	var tagsArray = hashtags.split(" ");
+
+	for(var tag_index in tagsArray){
+		if(tagsArray[tag_index].charAt(0) != '#')
+			tagsArray[tag_index] = "#" + tagsArray[tag_index];
+		console.log('Here is number ' + tag_index + ': ' + tagsArray[tag_index]);
+	}
+
+	var query = tagsArray.join(" ");
+		query += " since:" + date;
+		query += " near:Burlington, VT";
+		query += " within:" + radius + "mi";
+
+	console.log(query);
+	//query = encodeURIComponent(query);
+	//console.log(query);
+
+
+	T.get('search/tweets', { q: query, count: 100 }, function(err, reply) {
+		console.log(util.inspect(reply));
+		console.log('Error: ' + err);
  		res.render('index', {
  			reply: reply,
  			formSubmitted: true,
