24608a0a795cfb06dec41fff2575627c4f6f4272
pHelics <contact@phelics.com>
2013-11-30 01:24:23 -0500

    Hashtag input field validation

--- public/js/form.js
+++ public/js/form.js
@@ -1,9 +1,31 @@
 $(document).ready(function(){
 
+	var replaceLastWord = function(inputString,newstring) {
+		
+		var lastWord = $("input#hashtags").val().split(/[\s-]/).pop(),
+			newLastWord = lastWord;
+
+		// Add # if not the first character
+		if(lastWord.substring(0,1) != '#')
+					newLastWord = '#' + lastWord;
+
+		inputString = inputString.replace( lastWord, newLastWord );
+
+		return inputString;
+	};
+
 	// Hashtags input validation 
-	$("#hashtags").keypress(function(event){
-		if(event.keyCode == 32)
-			console.log('Space pressed');
+
+	// Change the last hashtag after one word is typed
+	$("input#hashtags").keypress(function(event){
+		if(event.keyCode == 32){
+			$("input#hashtags").val( replaceLastWord($("input#hashtags").val(), 'da') );
+		}
+	});
+
+	// Change the last hashtag when input looses focus
+	$("input#hashtags").blur(function(){
+		$("input#hashtags").val( replaceLastWord($("input#hashtags").val(), 'da') );
 	});
 
 });
