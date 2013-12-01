a861c81586a88c7ccc9771e182dd6f79acc30549
pHelics <contact@phelics.com>
2013-11-30 22:43:11 -0500

    Maps init and user geolocation

--- public/js/google_map.js
+++ public/js/google_map.js
@@ -1,4 +1,66 @@
 var map;
+
+$(document).ready(function(){
+	
+	initGoogleMaps();
+	setMapLocationToUser();
+	
+});
+
+// Initialize Google Maps
+var initGoogleMaps = function(){
+		map = new GMaps({
+        el: '#googleMap',
+        lat: 44.4758,
+        lng: -73.2119,
+        zoom: 12,
+        zoomControl : true,
+        zoomControlOpt: {
+            style : 'SMALL',
+            position: 'TOP_LEFT'
+        },
+        panControl : false,
+        streetViewControl : false,
+        mapTypeControl: false,
+        overviewMapControl: false
+      });
+};
+
+// Set the location of the map to user Geolocation
+var setMapLocationToUser = function(){
+	GMaps.geolocate({
+  		success: function(position) {
+    		map.setCenter(position.coords.latitude, position.coords.longitude);
+  		},
+  		error: function(error) {
+    		alert('Geolocation failed: '+error.message);
+  		},
+  		not_supported: function() {
+    		alert("Your browser does not support geolocation");
+  		},
+  		always: function() {
+    		
+  		}
+	});
+};
+
+var getGeoFromAddress = function(address){
+	GMaps.geocode({
+  		address: $('#address').val(),
+  		callback: function(results, status) {
+    		if (status == 'OK') {
+      			var latlng = results[0].geometry.location;
+      			map.setCenter(latlng.lat(), latlng.lng());
+      			map.addMarker({
+        			lat: latlng.lat(),
+        			lng: latlng.lng()
+      			});
+    		}
+  		}
+	});
+};
+
+/*
 function initialize() {
   var mapOptions = {
     zoom: 11,
@@ -9,3 +71,4 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+*/
