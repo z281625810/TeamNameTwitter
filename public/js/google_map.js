a36d391b291332a7a7a7ac35f7cb311855631bf0
pHelics <contact@phelics.com>
2013-12-02 03:09:32 -0500

    Clusters displayed on the map

--- public/js/google_map.js
+++ public/js/google_map.js
@@ -1,58 +1,25 @@
 var map,
-    markerCluster = null;
+    markerCluster = null,
+    markers = [];
 
 $(document).ready(function(){
 
 	initGoogleMaps();
-  //map.setCenter(position.coords.latitude, position.coords.longitude);
 
+  addUserLocation();
 /*
-  getLocation(user_location, function(location){
-    var latlng = new Object();
-    latlng['lat'] = location.latitude;
-    latlng['lng'] = location.longitude;
-    console.log(latlng);
-    
-  });
-*/
-  var markers = [];
   for (var i = 0; i < 100; ++i) {
     var latlng = new google.maps.LatLng(38.4758, -93.2119);
     var marker = new google.maps.Marker({position: latlng});
     markers.push(marker);
   };
+  */
 
-  markerCluster = new MarkerClusterer(map, markers);
-
-  /*
-  getLocation(user_location, function(coords){
-    latlng['lat'] = coords.latitude;
-    latlng['lng'] = coords.longitude;
-    console.log(coords);
-  });
-*/
 });
 
 // Initialize Google Maps
 var initGoogleMaps = function(){
-  /*
-		map = new GMaps({
-        el: '#googleMap',
-        lat: 38.4758,
-        lng: -93.2119,
-        //zoom: 12,
-        zoom: 4,
-        zoomControl : true,
-        zoomControlOpt: {
-            style : 'SMALL',
-            position: 'TOP_LEFT'
-        },
-        panControl : false,
-        streetViewControl : false,
-        mapTypeControl: false,
-        overviewMapControl: false
-      });
-*/
+
     var UScenter = new google.maps.LatLng(38.4758, -93.2119);
 
     map = new google.maps.Map(document.getElementById("googleMap"), 
@@ -71,43 +38,23 @@ var initGoogleMaps = function(){
     });  
 };
 
-var getUserGeolocation = function(){
+function addUserLocation() {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition( function(position) {
+      var user_location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      var user_marker = new google.maps.Marker({position: user_location});
 
-  GMaps.geolocate({
-    success: function(position) {
-      user_location['lat'] = position.coords.latitude;
-      user_location['lng'] = position.coords.longitude;
-    },
-    error: function(error) {
-      alert('Geolocation failed: '+error.message);
-    },
-    not_supported: function() {
-      alert("Your browser does not support geolocation");
-    },
-    always: function() {
-    
-    }
-  });
-
+      markers.push(user_marker);
+      // Call the next
+      addSearchLocation('Burlington');
+    });
+  }
 }
 
-function getLocation(loc, fn){
+function addSearchLocation(address){
+  drawMarkers();
+}
 
-  GMaps.geolocate({
-    success: function(position) {
-      //user_location['lat'] = position.coords.latitude;
-      //user_location['lng'] = position.coords.longitude;
-
-      fn(position.coords);
-    },
-    error: function(error) {
-      alert('Geolocation failed: '+error.message);
-    },
-    not_supported: function() {
-      alert("Your browser does not support geolocation");
-    },
-    always: function() {
-    
-    }
-  });
+function drawMarkers(){
+ markerCluster = new MarkerClusterer(map, markers, {zoomOnClick: false}); 
 }
