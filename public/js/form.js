d4c57bb1e6b0f290bd3ff716dda6b5b0de1842e8
pHelics <contact@phelics.com>
2013-12-02 19:25:22 -0500

    Only 2 or more hashtags allowed now

--- public/js/form.js
+++ public/js/form.js
@@ -1,35 +1,5 @@
 $(document).ready(function(){
 
-	/*
-	var replaceLastWord = function(inputString) {
-		
-		var lastWord = $("#inputhashtags").val().split(/[\s-]/).pop(),
-			newLastWord = lastWord;
-
-		// Add # if not the first character
-		if(lastWord.substring(0,1) != '#')
-					newLastWord = '#' + lastWord;
-
-		inputString = inputString.replace( $("input#hashtags").val().split(/[\s-]/).pop(), newLastWord );
-
-		return inputString;
-	};
-
-	// Hashtags input validation 
-
-	// Change the last hashtag after one word is typed
-	$("input#hashtags").keypress(function(event){
-		if(event.keyCode == 32){
-			$("input#hashtags").val( replaceLastWord($("input#hashtags").val() ) );
-		}
-	});
-
-	// Change the last hashtag when input looses focus
-	$("input#hashtags").blur(function(){
-		$("input#hashtags").val( replaceLastWord($("input#hashtags").val() ) );
-	});
-	*/
-
 	// Get today's date
 	var today = new Date();
 	// Set default date on form for two weeks ago
@@ -50,18 +20,29 @@ $(document).ready(function(){
 	});	
 
 	$("input#hashtags").blur(function(){
-
+		// Trim the spaces
 		$("input#hashtags").val($.trim( $("input#hashtags").val() ));
 
-		var hashtagInputs = $("input#hashtags").val().split(' ');
-		console.log('You entered ' + hashtagInputs.length + ' hashtags');
+		//if ( var hashtagInputs = $("input#hashtags").val().split(' ').length < 2 )
+			console.log('You entered ' + hashtagInputs.length + ' hashtags');
 
 		if(hashtagInputs.length < 2){
-			//$("<div class='alert alert-danger'>Please enter at least 2 hashtags</div>").insertAfter(("input#hashtags"));
+			
 		}
 	});
 
+		var hashes = $("input#hashtags").val().split(' ').length;
+		console.log("How many hashtags" + hashes);
 
+	$("#tweets_form").submit(function(event){
+
+		var hashes = $("input#hashtags").val().split(' ').length;
+		console.log("How many hashtags" + hashes);
+		if ( $("input#hashtags").val().split(' ').length < 2 ){
+			alert('Please enter at least 2 hashtags');
+			event.preventDefault();
+		}
+	});
 
 });
 
